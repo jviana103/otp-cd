6233578b77057305dd38f60aeed7d0f6ba54a770
Tomas-ns <a50458@alunos.isel.pt>
2026-04-01 14:21:54 +0100

    English support

--- app/src/main/java/pt/isel/MainActivity.kt
+++ app/src/main/java/pt/isel/MainActivity.kt
@@ -3,12 +3,12 @@ package pt.isel
 import android.Manifest
 import android.content.Context
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -44,7 +44,7 @@ import pt.isel.ui.theme.FirstAppTheme
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
